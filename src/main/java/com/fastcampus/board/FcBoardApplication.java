0869d55dd20859161bd33af6297aacf704ac0ab0
Youngju-Jang <joj1043@github.com>
2022-10-13 09:21:43 +0900

    last commit

--- src/main/java/com/fastcampus/board/FcBoardApplication.java
+++ src/main/java/com/fastcampus/board/FcBoardApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FcBoardApplication {
     public static void main(String[] args) {
         SpringApplication.run(FcBoardApplication.class, args);
-        System.out.println("test용");
+        System.out.println("test용22");
     }
         
 }
