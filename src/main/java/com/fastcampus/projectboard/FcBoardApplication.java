e26f71f401e240cb66d6fe0a0a98a09b160f5d3a
Youngju-Jang <joj1043@github.com>
2022-10-24 19:07:55 +0900

    #15 - thymeleaf 3 decoupled logic 설정
    java code로 thymeleaf 3 decoupled logic을 사용자 application property 로 제어할 수 있게 설정 추가
    https://gist.github.com/djkeh/6e1d557ce8c466135b1541d342b1c25c

--- src/main/java/com/fastcampus/projectboard/FcBoardApplication.java
+++ src/main/java/com/fastcampus/projectboard/FcBoardApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.ConfigurableApplicationContext;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class FcBoardApplication {
     public static void main(String[] args) {
